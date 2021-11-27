a992b375b488115192692bee8a2ce4252d690224
Alexander Sobrepera Toledo <81338127+AST060809@users.noreply.github.com>
2021-11-26 22:40:14 -0400

    Update Blockchain.py

--- Modulo 1 Crear una cadana de bloques/Blockchain.py
+++ Modulo 1 Crear una cadana de bloques/Blockchain.py
@@ -5,3 +5,36 @@ Created on Fri Nov 26 21:33:48 2021
 @author: Alexander
 """
 
+# Modulo 1 Como crear una cadena de bloques
+
+# Para instalar:
+# flask ==0.12.2: pip install Flask==0.12.2
+# Cliente HTTP Postman: https://www.getpostman.com/
+
+# Importar las librerias
+
+import datetime #para manejar fechas
+import hashlib # es la libreria para crear los hash
+import json #para crear objetos json
+from flask import Flask, jsonify
+
+#Parte 1 Crear la Cadena de Bloques
+
+class Blockchain:
+    
+    def __init__(self): #creando el constructor, el parametro self hace referencia al objeto creado, es como el this en JS
+        self.chain = [] #creando el objeto chain
+        self.create_block(proof = 1, prev_hash = '0')
+        
+    def create_block(self, proof, prev_hash):
+        block = {
+            
+            }
+        
+    
+     
+    
+    
+
+
+#Parte 2 Minado de un bloque de la Cadena
