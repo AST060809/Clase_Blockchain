e80d0d4440e645aba6a32bc284470ce8fb210f79
Alexander Sobrepera Toledo <81338127+AST060809@users.noreply.github.com>
2021-11-27 22:03:24 -0400

    Update Blockchain.py

--- Modulo 1 Crear una cadana de bloques/Blockchain.py
+++ Modulo 1 Crear una cadana de bloques/Blockchain.py
@@ -24,14 +24,57 @@ class Blockchain:
     
     def __init__(self): #creando el constructor, el parametro self hace referencia al objeto creado, es como el this en JS
         self.chain = [] #creando el objeto chain en una lista vacia
-        self.create_block(proof = 1, prev_hash = '0')# Invocando la funcion crear bloque dentro del contructor, se le pasn los parametros del primer bloque.
+        self.create_block(proof = 1, prev_hash = '0')# Invocando la función crear bloque dentro del constructor, se le pasan los parametros del primer bloque.
         
-    def create_block(self, proof, prev_hash):
-        block = {
-            
+    def create_block(self, proof, prev_hash):  # la proof en la preuba de trabajo, demostrando que el bloque fue minado. LLamando asi a la funcion despues de haber minado el bloque.
+        block = {'index': len(self.chain)+1,
+                 'timestamp': str(datetime.datetime.now()), # esto me devuelve la fecha en la que fue minado el bloque. (llamamos a la libreira datatime, con la funcion datetime y el metodo now()), le hago un casting a string ya que lo voy aguardar en formato json.
+                 'proof':proof, # la prueba de trabajo que se creo el bloque.
+                 'prev_hash':prev_hash      
             }
-        
+        self.chain.append(block) #añadimos el bloque a la cadena)
+        return block
+    def get_prev_block(self):
+        return self.chain[-1] # -1 para obtener el ultimo elemento de la cadena, en este caso el hash de la anterior.
     
+    def proof_of_work(self, prev_proof): # función que vamos a utilizar para comprobar que el valor es valido para minar el bloque
+        new_proof = 1 # esta variable la utilizaremos para determinar si es la solución y se ira incrementando de 1 en 1
+        check_proof = False # caundo se haya resuelto el problema crytografivo valdra True.
+        while check_proof is False:
+            hash_operation = hashlib.sha256(str(new_proof**2 - prev_proof**2).encode()).hexdigest() #esta varaible sera un string de 64 caracteres. encode() verifica que el formato es el adecuado.
+            if hash_operation[:4]=='0000':
+                check_proof = True
+            else:
+                new_proof += 1
+        return new_proof
+    
+    # Función para dependiendo de un bloque calcular su hash
+    def hash (self, block):
+        encoded_block = json.dumps(block, sort_keys= True).encode() # utilizamos esta variable para guardar el volcado de el diccionario a un objeto json. el parametro sort_keys es para ordenar las claves del diccionario.
+        return hashlib.sha256(encoded_block).hexdigest()
+    
+    # Función para validar si la cadena es valida
+    def is_chain_valid(self, chain):
+        prev_block = chain[0]
+        block_index = 1
+        while block_index < len(chain):
+            block = chain[block_index]
+            if block['prev_hash'] != self.hash(prev_block):
+                return False
+            # El valor de la prueba de trabajo del bloqur actual y del previo.
+            prev_proof = prev_block['proof']
+            proof = block['proof']
+            hash_operation = hashlib.sha256(str(proof**2 - prev_proof**2).encode()).hexdigest()
+            if hash_operation[:4]!='0000':
+                 return False
+            prev_block = block
+            block_index +=1
+        return True
+    
+                
+                
+
+                    
      
     
     
